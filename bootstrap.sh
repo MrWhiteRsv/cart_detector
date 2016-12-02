4c0e7cf0114bcb206538ee35cfbee395cc59d959
MrWhiteRsv <whitersrvdg@gmail.com>
2016-12-02 02:38:56 +0000

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
-sudo apt install -y gpsd 
+sudo apt install -y python-dev 
+sudo apt install -y gpsd  gpsd-clients
 sudo pip install gps3
 sudo apt install -y mosquitto-clients 
 sudo pip install paho-mqtt
 sudo pip install envirophat
+sudo pip install unicornhat
+
+sudo apt install -y bluez python-bluez python-numpy
+sudo apt install -y python-smbus
