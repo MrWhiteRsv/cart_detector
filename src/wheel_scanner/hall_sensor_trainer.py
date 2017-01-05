3de9c0ebad9ce6aa1770a57a04fab4629b6dc0e0
MrWhiteRsv <whitersrvdg@gmail.com>
2017-01-05 21:16:08 +0000

    Test training on existing file.

--- src/wheel_scanner/hall_sensor_trainer.py
+++ src/wheel_scanner/hall_sensor_trainer.py
@@ -1,10 +1,16 @@
+import collections
+
 class HallSensorTrainer():
 
+  activeBuffer = None
+  
+  def __init__(self):
+    MAX_BUFFER_SIZE = 10000
+    self.activeBuffer = collections.deque(maxlen = MAX_BUFFER_SIZE)
+
   def add_sample(self, value):
-    assert False, 'HallSensorTrainer.add_sample not implemented yet!'
-    
-  def get_activivity_threshold(self):
-    assert False, 'HallSensorTrainer.get_activivity_threshold not implemented yet!'
+    print (value, len(self.activeBuffer))
+    self.activeBuffer.append(value)
     
   def get_high_low_threshold(self):
     assert False, 'HallSensorTrainer.get_high_low_threshold not implemented yet!'
--- test/wheel_scanner/hall_sensor_trainer_test.py
+++ test/wheel_scanner/hall_sensor_trainer_test.py
@@ -1,11 +1,24 @@
 from src.wheel_scanner import hall_sensor_trainer
 
+import os
+import json
+
 class HallSensorTrainerTest():
 
   def test(self):
     try:
+      dir = os.path.dirname(__file__)
+      file_name = os.path.join(dir, '../../runs/v3.dat')
+      test_file = open(file_name, 'r')
       trainer = hall_sensor_trainer.HallSensorTrainer()
-      trainer.add_sample(5)
+      for line in test_file:
+        try: 
+          parsed = json.loads(line)
+        except Exception as inst:
+          print ('parsing err')
+          continue
+        trainer.add_sample(parsed['val_0'])
+
     except Exception as inst:
       print (inst)
       return False
