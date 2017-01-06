0aef06aa7ade5d2537a868dd970d04f26dd7485b
MrWhiteRsv <whitersrvdg@gmail.com>
2017-01-06 13:19:01 -0800

    Do not pass index to trainer.

--- src/wheel_scanner/hall_sensor_trainer.py
+++ src/wheel_scanner/hall_sensor_trainer.py
@@ -4,8 +4,9 @@ from .. utils import logger
 
 class HallSensorTrainer():
 
-  logger = None
   activeBuffer = None
+  logger = None
+  sample_index = None
 
   def __init__(self):
     MAX_ACTIVE_BUFFER_SIZE = 10000
@@ -13,14 +14,16 @@ class HallSensorTrainer():
     self.logger = logger.Logger()
     self.logger.open(mqtt_log_file_name = None, log_to_mqtt_file = True,
         log_to_mqtt = False, log_to_stdout = True)
+    self.sample_index = 0
 
-  def add_sample(self, value, logging_index):      
+  def add_sample(self, value):      
     self.activeBuffer.append(value)
+    self.sample_index = self.sample_index + 1
     if (len(self.activeBuffer) > 2):
       val_n = self.activeBuffer[-1]
       val_nm1 = self.activeBuffer[-2]
       val_nm2 = self.activeBuffer[-3]
-      self.logger.log_training_reading(val_n, logging_index)
+      self.logger.log_training_reading(val_n, self.sample_index)
 
   def get_high_low_threshold(self):
    
--- test/wheel_scanner/hall_sensor_trainer_test.py
+++ test/wheel_scanner/hall_sensor_trainer_test.py
@@ -10,29 +10,17 @@ class HallSensorTrainerTest():
     try:
       dir = os.path.dirname(__file__)
       input_file_name = os.path.join(dir, '../runs/run_0.dat')
-      v0_output_file_name = os.path.join(dir, '../gen/gen_v0.txt')
-      v1_output_file_name = os.path.join(dir, '../gen/gen_v1.txt')
-      #extreme_output_file_name = os.path.join(dir, '../../test/gen/gen_ext.txt')
       input_file = open(input_file_name, 'r')
-      v0_output_file = open(v0_output_file_name, 'w')
-      v1_output_file = open(v1_output_file_name, 'w')
       trainer = hall_sensor_trainer.HallSensorTrainer()
-      logging_index = 0
       for line in input_file:
         try: 
           parsed = json.loads(line)
         except Exception as inst:
-          print parsed
-          print ('parsing err')
+          print ('Parsing err, skipped line.')
           continue
         if (parsed['topic'] == 'cart/cartId/hall_reading'):
-          logging_index = logging_index + 1
-          trainer.add_sample(parsed['val_0'], logging_index)
-          v0_output_file.write(str(logging_index) + ', ' + str(parsed['val_0']) + '\n')
-          v1_output_file.write(str(logging_index) + ', ' + str(parsed['val_1']) + '\n')
+          trainer.add_sample(parsed['val_0'])
       input_file.close()
-      v0_output_file.close()
-      v1_output_file.close()
       
     except Exception as inst:
       print (inst)
