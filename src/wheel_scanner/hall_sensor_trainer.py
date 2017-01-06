01dcd77329746bfcc9ac8c3db7e1769fc68ebcd4
MrWhiteRsv <whitersrvdg@gmail.com>
2017-01-06 13:06:44 -0800

    Logging trainer activity.

--- src/wheel_scanner/hall_sensor_trainer.py
+++ src/wheel_scanner/hall_sensor_trainer.py
@@ -1,16 +1,33 @@
 import collections
 
+from .. utils import logger
+
 class HallSensorTrainer():
 
+  logger = None
   activeBuffer = None
-  
-  def __init__(self):
-    MAX_BUFFER_SIZE = 10000
-    self.activeBuffer = collections.deque(maxlen = MAX_BUFFER_SIZE)
 
-  def add_sample(self, value):
-    print (value, len(self.activeBuffer))
+  def __init__(self):
+    MAX_ACTIVE_BUFFER_SIZE = 10000
+    self.activeBuffer = collections.deque(maxlen = MAX_ACTIVE_BUFFER_SIZE)
+    self.logger = logger.Logger()
+    self.logger.open(mqtt_log_file_name = None, log_to_mqtt_file = True,
+        log_to_mqtt = False, log_to_stdout = True)
+
+  def add_sample(self, value, logging_index):      
     self.activeBuffer.append(value)
-    
+    if (len(self.activeBuffer) > 2):
+      val_n = self.activeBuffer[-1]
+      val_nm1 = self.activeBuffer[-2]
+      val_nm2 = self.activeBuffer[-3]
+      self.logger.log_training_reading(val_n, logging_index)
+
   def get_high_low_threshold(self):
+   
+    # find the the delta between extreme values
+    # find the histogram of deltas
+    # find the bigest threshold between histogram sorted values
+    # find the set of 'clean' min max values
+    # find a threshold based on these min / max values
     assert False, 'HallSensorTrainer.get_high_low_threshold not implemented yet!'
+
--- test/wheel_scanner/hall_sensor_trainer_test.py
+++ test/wheel_scanner/hall_sensor_trainer_test.py
@@ -1,25 +1,42 @@
-from src.wheel_scanner import hall_sensor_trainer
-
-import os
 import json
+import os
+import traceback
+
+from src.wheel_scanner import hall_sensor_trainer
 
 class HallSensorTrainerTest():
 
   def test(self):
     try:
       dir = os.path.dirname(__file__)
-      file_name = os.path.join(dir, '../../runs/v3.dat')
-      test_file = open(file_name, 'r')
+      input_file_name = os.path.join(dir, '../runs/run_0.dat')
+      v0_output_file_name = os.path.join(dir, '../gen/gen_v0.txt')
+      v1_output_file_name = os.path.join(dir, '../gen/gen_v1.txt')
+      #extreme_output_file_name = os.path.join(dir, '../../test/gen/gen_ext.txt')
+      input_file = open(input_file_name, 'r')
+      v0_output_file = open(v0_output_file_name, 'w')
+      v1_output_file = open(v1_output_file_name, 'w')
       trainer = hall_sensor_trainer.HallSensorTrainer()
-      for line in test_file:
+      logging_index = 0
+      for line in input_file:
         try: 
           parsed = json.loads(line)
         except Exception as inst:
+          print parsed
           print ('parsing err')
           continue
-        trainer.add_sample(parsed['val_0'])
-
+        if (parsed['topic'] == 'cart/cartId/hall_reading'):
+          logging_index = logging_index + 1
+          trainer.add_sample(parsed['val_0'], logging_index)
+          v0_output_file.write(str(logging_index) + ', ' + str(parsed['val_0']) + '\n')
+          v1_output_file.write(str(logging_index) + ', ' + str(parsed['val_1']) + '\n')
+      input_file.close()
+      v0_output_file.close()
+      v1_output_file.close()
+      
     except Exception as inst:
       print (inst)
+      traceback.print_exc()
       return False
+      
     return True
