7c5cd760fa2693a7026b5b9058c2e539b9e7aa01
MrWhiteRsv <whitersrvdg@gmail.com>
2017-01-18 18:03:03 -0800

    1. new moniotr functionality. 2. tester for the monitor.

---
+++ monitor_test.py
@@ -0,0 +1,27 @@
+# Responsible for starting and stopping the different scanners 
+
+import getopt
+import sys
+import time
+
+import src.utils.monitor
+import src.utils.monitor
+from src.utils.colors import Colors
+monitor = src.utils.monitor
+
+def main(argv):
+ 
+  monitor.init()
+  monitor.clear()
+  monitor.show_beacon_on(Colors.PURPLE)
+  monitor.show_quality(Colors.YELLOW)
+  monitor.show_show_arrow_bottom()
+  for val in range(64):
+    monitor.show_counter_0(val)
+    monitor.show_counter_1(val / 2)
+    time.sleep(0.1)
+  time.sleep(5)
+  
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
--- src/utils/monitor.py
+++ src/utils/monitor.py
@@ -1,4 +1,108 @@
+from colors import Colors
 import unicornhat as unicorn
+
+def init():
+    unicorn.set_layout(unicorn.AUTO)
+    unicorn.rotation(0)
+    unicorn.brightness(0.5)
+    
+def clear():
+  for row in range(8):
+    for col in range(4):
+      set_pixel(row, col, Colors.BLACK)
+  unicorn.show()
+  
+def show_counter_0(val):
+  # print 'monitor half revolution:', counter
+  set_pixel(0, 0, Colors.BLACK if val & 1 == 0 else Colors.RED)
+  set_pixel(0, 1, Colors.BLACK if val & 2 == 0 else Colors.RED)
+  set_pixel(1, 0, Colors.BLACK if val & 4 == 0 else Colors.RED)
+  set_pixel(1, 1, Colors.BLACK if val & 8 == 0 else Colors.RED)
+  set_pixel(2, 0, Colors.BLACK if val & 16 == 0 else Colors.RED)
+  set_pixel(2, 1, Colors.BLACK if val & 32 == 0 else Colors.RED)
+  unicorn.show()
+  
+def show_counter_1(val):
+  # print 'monitor half revolution:', counter
+  set_pixel(0, 2, Colors.BLACK if val & 1 == 0 else Colors.GREEN)
+  set_pixel(0, 3, Colors.BLACK if val & 2 == 0 else Colors.GREEN)
+  set_pixel(1, 2, Colors.BLACK if val & 4 == 0 else Colors.GREEN)
+  set_pixel(1, 3, Colors.BLACK if val & 8 == 0 else Colors.GREEN)
+  set_pixel(2, 2, Colors.BLACK if val & 16 == 0 else Colors.GREEN)
+  set_pixel(2, 3, Colors.BLACK if val & 32 == 0 else Colors.GREEN)
+  unicorn.show()
+  
+def show_beacon_on(color):
+  for row in range(3, 5):
+    for col in range(0, 4):
+      set_pixel(row, col, color)
+  unicorn.show()
+  
+def show_beacon_off():
+  for row in range(3, 4):
+    for col in range(0, 3):
+      set_pixel(row, col, Colors.BLACK)
+  unicorn.show()
+  
+def show_quality(color):
+  for row in range(5, 8):
+    col = 3
+    set_pixel(row, col, color)
+  unicorn.show()  
+
+def clear_direction():
+  for row in range(5, 8):
+    for col in range(0, 3):
+      set_pixel(row, col, Colors.BLACK)
+  unicorn.show()
+  
+def clear_direction_stop():
+  for row in range(5, 8):
+    for col in range(0, 3):
+      set_pixel(row, col, Colors.RED)
+  unicorn.show()
+      
+def show_show_arrow_right():
+  color = Colors.YELLOW
+  clear_direction()
+  set_pixel(6, 0, color)
+  set_pixel(6, 1, color)
+  set_pixel(6, 2, color)
+  set_pixel(7, 1, color)
+  unicorn.show()
+  
+def show_show_arrow_left():
+  color = Colors.YELLOW
+  clear_direction()
+  set_pixel(6, 0, color)
+  set_pixel(6, 1, color)
+  set_pixel(6, 2, color)
+  set_pixel(5, 1, color)
+  unicorn.show()
+  
+def show_show_arrow_top():
+  color = Colors.YELLOW
+  clear_direction()
+  set_pixel(5, 1, color)
+  set_pixel(6, 1, color)
+  set_pixel(7, 1, color)
+  set_pixel(6, 0, color)
+  unicorn.show()
+  
+def show_show_arrow_bottom():
+  color = Colors.YELLOW
+  clear_direction()
+  set_pixel(5, 1, color)
+  set_pixel(6, 1, color)
+  set_pixel(7, 1, color)
+  set_pixel(6, 2, color)
+  unicorn.show()
+""" internals """   
+def set_pixel(row, col, rgb_color):
+  unicorn.set_pixel(row, col,
+      rgb_color.value[0], rgb_color.value[1], rgb_color.value[2])
+    
+""" Deprecated """
 
 class Monitor():
   
@@ -7,16 +111,6 @@ class Monitor():
     unicorn.rotation(0)
     unicorn.brightness(0.5)
      
-  def rgb_of_color(self, color):
-    return {
-        'red': (200, 30, 30),
-        'green': (30, 200, 30),
-        'blue': (30, 30, 200),
-        'black': (0, 0, 0),
-        'yellow': (255, 255, 80),
-        'purple': (128,0,128),
-    }.get(color, (255, 255, 255))
-    
   def set_pixel(self, row, col, color):
      (r, g, b) = self.rgb_of_color(color);
      unicorn.set_pixel(row, 3 - col, r, g, b)
@@ -38,3 +132,19 @@ class Monitor():
     self.set_pixel(2, 3, 'black' if counter & 64 == 0 else 'yellow')
     self.set_pixel(3, 3, 'black' if counter & 128 == 0 else 'yellow')
     unicorn.show()
+    
+  """ Internals. """
+  def set_pixel(self, row, col, color):
+     (r, g, b) = self.rgb_of_color(color);
+     unicorn.set_pixel(row, 3 - col, r, g, b)
+     
+  #deprecated
+  def rgb_of_color(self, color):
+    return {
+        'red': (200, 30, 30),
+        'green': (30, 200, 30),
+        'blue': (30, 30, 200),
+        'black': (0, 0, 0),
+        'yellow': (255, 255, 80),
+        'purple': (128,0,128),
+    }.get(color, (255, 255, 255))
