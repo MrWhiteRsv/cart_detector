e97f9daa08753ddf30aa91c44855e9c22fc17ff4
MrWhiteRsv <whitersrvdg@gmail.com>
2017-01-19 10:00:28 -0800

    Colors file.

---
+++ src/utils/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Colors(Enum):
+  RED = (200, 30, 30)
+  GREEN = (30, 200, 30)
+  BLUE = (30, 30, 200)
+  BLACK = (0, 0, 0)
+  YELLOW = (255, 255, 80)
+  PURPLE = (128, 0, 128)
+  WHITE =  (255, 255, 255)
